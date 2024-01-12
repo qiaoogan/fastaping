1a414736e8ad5d176281f3602b0f963ffde2e0ec
qiaoogan <qiaogang1984@163.com>
2024-01-12 14:55:25 +0800

    env

--- jenkins.sh
+++ jenkins.sh
@@ -6,5 +6,5 @@ docker build --tag=backendpy:0.0.${BUILD_NUMBER} .
 
 # deploy
 export FASTAPING_VERSON=0.0.${BUILD_NUMBER}
-kubectl apply -f ./deployment/deploysvc.yaml --env=FASTAPING_VERSION=0.0.${BUILD_NUMBER}
+kubectl apply -f ./deployment/deploysvc.yaml
 # kubectl set image deployment/ezl-backendpy-depl ezl-backendpy=backendpy:0.0.${BUILD_NUMBER}
