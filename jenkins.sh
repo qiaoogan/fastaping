c687a043ecdab4d0e2746bf9a56439c6406f37c8
qiaoogan <qiaogang1984@163.com>
2024-01-12 16:25:11 +0800

    update

--- jenkins.sh
+++ jenkins.sh
@@ -10,4 +10,5 @@ kubectl patch configmap/fastaping-config \
     --type merge \
     -p "{\"data\":{\"FASTAPING_VERSION\":\"0.0.${BUILD_NUMBER}\"}}"
 kubectl apply -f ./deployment/deploysvc.yaml
+kubectl set image deployment/ezl-backendpy-depl ezl-backendpy=backendpy:0.0.${BUILD_NUMBER}
 
