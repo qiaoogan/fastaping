6e91a9b63a3a5aad2cbb576f3ecf76e07dd2cc53
qiaoogan <qiaogang1984@163.com>
2024-03-05 19:23:56 +0800

    debugging ci

--- API_testing/test-in-container-cloud.sh
+++ API_testing/test-in-container-cloud.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/sh
 
 docker container run \
   --rm \
@@ -6,4 +6,4 @@ docker container run \
   -e TEST_ENV="container" \
   -e TEST_HOST="http://123.60.93.173/fastapi" \
   qiaoogan/papitestenv:0.0.2 \
-  bash /home/mount/tests-entry.sh
+  sh /home/mount/tests-entry.sh
--- API_testing/test-in-container-local.sh
+++ API_testing/test-in-container-local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker container run \
   --rm \
@@ -6,4 +6,4 @@ docker container run \
   -e TEST_ENV="container" \
   -e TEST_HOST="http://192.168.2.104:8901" \
   qiaoogan/papitestenv:0.0.2 \
-  bash /home/mount/tests-entry.sh
+  sh /home/mount/tests-entry.sh
