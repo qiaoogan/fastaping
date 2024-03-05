a7ba2b8029ebee5b44882a6be3875445ae4b686f
qiaoogan <qiaogang1984@163.com>
2024-03-05 21:09:41 +0800

    update health checking for api tests

--- API_testing/health_check.py
+++ API_testing/health_check.py
@@ -4,6 +4,7 @@ from clients.TestClient import get_health
 while True:
     response = get_health()
     if response.status_code == 200:
+        print("checking health ... passed")
         break
     else:
         print("checking health ...")
