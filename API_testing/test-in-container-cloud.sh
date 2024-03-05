8f84b871d503466762d1d3bd3f78dd8c0d34bdf8
qiaoogan <qiaogang1984@163.com>
2024-03-05 16:25:53 +0800

    debugging ci

--- API_testing/test-in-container-cloud.sh
+++ API_testing/test-in-container-cloud.sh
@@ -2,6 +2,6 @@ docker container run \
   --rm \
   --mount type=bind,source="$(pwd)",target=/home/mount \
   -e TEST_ENV="container" \
-  -e TEST_HOST="http://hw.dogger.instance/fastapi" \
+  -e TEST_HOST="http://123.60.93.173/fastapi" \
   qiaoogan/papitestenv:0.0.1 \
   ash /home/mount/tests-entry.sh
