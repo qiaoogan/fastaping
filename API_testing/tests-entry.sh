5ce6522d37ea82cc3ace3f656788eb0baa397f0e
qiaoogan <qiaogang1984@163.com>
2024-03-06 16:10:31 +0800

    update entry to indicate test failure on CI

--- API_testing/tests-entry.sh
+++ API_testing/tests-entry.sh
@@ -2,7 +2,16 @@
 
 echo "tests ran in container"
 pytest -v --alluredir=./allure-results --clean-alluredir
+test_exit_code=$?
+
 rm -rf allure-report && pwd && ls -al
-echo $PATH
 allure generate allure-results -c -o allure-report
 pwd && ls -al
+last_exit_code=$?
+
+if [ $test_exit_code -ne 0 ] || [ $last_exit_code -ne 0 ]; then
+    echo "Test Failed"
+    exit 1
+else
+    echo "Test Completed"
+fi
