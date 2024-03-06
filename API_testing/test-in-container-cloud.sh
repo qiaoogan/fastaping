6a57af2495bfb64d6104f35f38a176bbb17999d9
qiaoogan <qiaogang1984@163.com>
2024-03-06 10:27:02 +0800

    update health checking for api tests

--- API_testing/test-in-container-cloud.sh
+++ API_testing/test-in-container-cloud.sh
@@ -4,6 +4,6 @@ docker container run \
   --rm \
   --mount type=bind,source="$(pwd)",target=/home/mount \
   -e TEST_ENV="container" \
-  -e TEST_HOST="http://host.docker.internal:31101" \
+  -e TEST_HOST="http://192.168.5.143:31101" \
   qiaoogan/papitestenv:0.0.3 \
   bash /home/mount/tests-entry.sh
