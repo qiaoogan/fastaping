0a91e8767e14f63eceea1b425b6d01b7b228bb68
qiaoogan <qiaogang1984@163.com>
2024-03-06 15:32:28 +0800

    update test running container

--- API_testing/health-check-cloud.sh
+++ API_testing/health-check-cloud.sh
@@ -5,5 +5,5 @@ docker container run \
   --mount type=bind,source="$(pwd)",target=/home/mount \
   -e TEST_ENV="container" \
   -e TEST_HOST="http://192.168.5.143:31101" \
-  qiaoogan/papitestenv:0.0.5 \
+  qiaoogan/papitestenv:0.0.6 \
   python /home/mount/health_check.py
--- API_testing/test_environment/push-image.sh
+++ API_testing/test_environment/push-image.sh
@@ -1,2 +1,2 @@
-docker tag papitestenv:latest qiaoogan/papitestenv:0.0.5
-docker push qiaoogan/papitestenv:0.0.5
+docker tag papitestenv:latest qiaoogan/papitestenv:0.0.6
+docker push qiaoogan/papitestenv:0.0.6
