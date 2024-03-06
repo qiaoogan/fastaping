02a8e265ba9e47de1f79c7b9a228c46771ea5faf
qiaoogan <qiaogang1984@163.com>
2024-03-06 10:46:05 +0800

    update health checking for api tests

--- API_testing/health-check-cloud.sh
+++ API_testing/health-check-cloud.sh
@@ -4,6 +4,6 @@ docker container run \
   --rm \
   --mount type=bind,source="$(pwd)",target=/home/mount \
   -e TEST_ENV="container" \
-  -e TEST_HOST="http://123.60.93.173/fastapi" \
+  -e TEST_HOST="http://192.168.5.143:31101" \
   qiaoogan/papitestenv:0.0.2 \
   python /home/mount/health_check.py
